6f43fc35b404e26196942e9e5689e7e3dc3bb116
Bonvic <bundi.bonvic@gmail.com>
2025-06-13 14:08:39 +0300

    init project

--- internal/queue/queue.go
+++
@@ -1,8 +0,0 @@
-package queue
-
-type TaskQueue interface {
-    Enqueue(job []byte)
-    Dequeue() (string, error)
-	XReadGeneric(lastID string, block int64, mapper func(map[string]interface{}) (any, error)) ([]any, error)
-    XDelete(id string) error
-}
