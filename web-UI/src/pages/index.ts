a8ad8053d90ab2d4a3eb1034de1555df97039ccc
Bonvic <bundi.bonvic@gmail.com>
2025-09-11 18:52:41 +0300

    remove web

--- web-UI/src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { Dashboard } from './Dashboard';
-export { Jobs } from './Jobs';
-export { JobDetails } from './JobDetails';
-export { CreateJob } from './CreateJob';
-export { JobRuns } from './JobRuns';
-export { JobRunDetails } from './JobRunDetails';
-export { Secrets } from './Secrets';
-export { Logs } from './Logs';
